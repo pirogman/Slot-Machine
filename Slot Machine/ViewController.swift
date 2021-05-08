bc5cdab7f1b19ef375694b332f121b91beca29dd
pirogman <pirogman@gmail.com>
2021-05-08 12:56:35 +0300

    Initial Commit

---
+++ Slot Machine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Slot Machine
+//
+//  Created by Alex Pirog on 08.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
